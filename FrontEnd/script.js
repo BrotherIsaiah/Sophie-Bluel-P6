2db70a92ffedc0476ec59b0fc45c9f8701522031
Isaiah <isaiah.ekue@protonmail.com>
2024-01-05 16:21:31 +0100

    Add buttons filter

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,22 +1,35 @@
 //Récupérer les travaux
 async function getWork () {
-console.log("test getWork");
+//console.log("test getWork");
 try {
     const reponseJSON = await fetch ("http://localhost:5678/api/works");
-    console.log(reponseJSON)
+    //console.log(reponseJSON)
     const reponseJS = await reponseJSON.json();
-    console.log(reponseJS);
+    //console.log(reponseJS);
     const divGallery = document.querySelector(".gallery")
-    console.log(divGallery)
+    //console.log(divGallery)
     reponseJS.forEach(work => {
         const figureWork = document.createElement("figure")
         const imageWork = document.createElement("img");
         imageWork.src = work.imageUrl;
         const titleWork = document.createElement("figcaption");
         titleWork.textContent = work.title;
-        divGallery.appendChild(figureWork)
-        figureWork.appendChild(imageWork)
-        figureWork.appendChild(titleWork)
+        divGallery.appendChild(figureWork);
+        figureWork.appendChild(imageWork);
+        figureWork.appendChild(titleWork);
+    })
+    const reponseCategory = await fetch ("http://localhost:5678/api/categories");
+    console.log(reponseCategory);
+    const reponseJSCat = await reponseCategory.json();
+    console.log(reponseJSCat);
+
+    reponseJSCat.forEach(category => {
+        const sectionPortfolio = document.getElementById("portfolio");
+        const objetCategory = document.createElement("input");
+        objetCategory.setAttribute("type", "submit");
+        objetCategory.value = category.name;
+        sectionPortfolio.appendChild(objetCategory);
+        sectionPortfolio.insertBefore(objetCategory, divGallery);
     })
 }
 catch (error){
@@ -25,3 +38,5 @@ catch (error){
 };
 
 getWork();
+
+
