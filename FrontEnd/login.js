e785e228eef864e48de795314fa378f9ba72b10b
Isaiah <isaiah.ekue@protonmail.com>
2024-01-09 22:55:33 +0100

    get id and token when form is submitted

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,2 +1,34 @@
-const loginForm = document.querySelector("#login form");
-console.log(loginForm);
+document.addEventListener("DOMContentLoaded", function() {
+    const connectButton = document.getElementById("connect");
+    console.log(connectButton);
+    const loginForm = document.querySelector("#login form");
+    console.log(loginForm);
+    const emailValue = "sophie.bluel@test.tld"
+    console.log(emailValue);
+    const passwordValue = "S0phie"
+    console.log(passwordValue);
+    connectButton.addEventListener("click", logIn);
+        console.log(connectButton);
+    
+    async function logIn (event){
+        event.preventDefault();
+        try {
+            const reponseLogin = await fetch ("http://localhost:5678/api/users/login", {
+                method: "POST",
+                headers: {
+                     "accept": "application/json",
+                     "Content-Type": "application/json"},
+                body: JSON.stringify({ "email": emailValue, "password": passwordValue})
+            });
+            const reponseData = await reponseLogin.json();
+            console.log(reponseData);
+
+            
+        } catch (error) {
+            console.log(error, "erreur")
+        }
+        console.log(logIn);
+    }
+});
+
+
