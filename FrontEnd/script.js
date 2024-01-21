aa523601efae54a8cbaee2447333fb8f14dbaf8f
Isaiah <isaiah.ekue@protonmail.com>
2024-01-21 23:13:49 +0100

    new branch to try out POST Method

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -150,7 +150,49 @@ async function fetchModal (){
         addPhotoButton.value = "Ajouter photo";
         modalDiv2.appendChild(addPhotoButton);
         addPhotoButton.addEventListener("click", async function () {
-            
+            modalDiv2.style.display = "none";
+            const modalDiv3 = document.querySelector(".modal3");
+            modalDiv3.style.display = "flex";
+        
+            const imageFileInput = document.createElement("input");
+            imageFileInput.type = "file";  // Pour les fichiers
+        
+            const titleInput = document.createElement("input");
+            titleInput.type = "text";
+        
+            const categoryInput = document.createElement("input");
+            categoryInput.type = "text";
+        
+            const formWork = document.createElement("form");
+            const formData = new FormData(formWork);
+            formData.append("image", imageFileInput.files[0]);  // Utilisez .files[0] pour obtenir le fichier à partir de l'input
+            formData.append("title", titleInput.value);
+            formData.append("category", categoryInput.value);
+        
+            try {
+                const storedToken = getToken();
+                const reponseJSON = await fetch("http://localhost:5678/api/works", {
+                    method: "POST",
+                    headers: {
+                        "Accept": "application/json",
+                        "Authorization": `Bearer ${storedToken}`,  // Ajoutez le token ici
+                    },
+                    body: formData,
+                });
+        
+                if (reponseJSON.ok) {
+                    const responseData = await reponseJSON.json();
+                    console.log("Travail créé", responseData);
+                    // Ajoutez ici le code pour traiter la réponse, si nécessaire
+                } else {
+                    console.error("Erreur lors de la création");
+                    // Ajoutez ici le code pour gérer les erreurs, si nécessaire
+                }
+            } catch (error) {
+                console.error("Erreur pendant la requête", error);
+            }
+        
+            modalDiv3.appendChild(formWork);
         });
         
         
